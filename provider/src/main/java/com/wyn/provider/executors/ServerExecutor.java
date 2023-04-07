8f7961d823cb2c7ad8a62832cb8542dcb280821d
IssacStudent <ynwang0125@gmail.com>
2023-04-07 08:37:08 +0800

    服务端执行器

---
+++ provider/src/main/java/com/wyn/provider/executors/ServerExecutor.java
@@ -0,0 +1,53 @@
+package com.wyn.provider.executors;
+
+import com.wyn.common.protocol.WProtocolModel;
+import com.wyn.common.serialize.KryoSerializer;
+import com.wyn.provider.invoker.ServiceInvoker;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class ServerExecutor implements Runnable {
+
+    private Socket socket;
+
+    private final int MAX_PACKAGE_SIZE = 10240;
+
+    private static final KryoSerializer KRYO_SERIALIZER = new KryoSerializer();
+
+    public ServerExecutor(Socket socket) {
+        this.socket = socket;
+        KRYO_SERIALIZER.setClazz(WProtocolModel.class);
+    }
+
+    @Override
+    public void run() {
+        InputStream is = null;
+        OutputStream os = null;
+
+        try {
+            is = socket.getInputStream();
+            os = socket.getOutputStream();
+            byte[] data = new byte[MAX_PACKAGE_SIZE];
+            byte[] resultData = new byte[MAX_PACKAGE_SIZE];
+            int len = is.read(data);
+            WProtocolModel protocolModel = KRYO_SERIALIZER.deserialize(data, 0, len);
+            Object result = ServiceInvoker.PROXY.process(protocolModel);
+            KRYO_SERIALIZER.serialize(result, resultData);
+            os.write(resultData);
+            os.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                os.close();
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
